170e643a72ee4f98aa9f0a71cd980e19bfdf42b5
NinisPaninis <nina-wolfisberg@hotmail.com>
2022-06-13 12:05:30 +0200

    Checkout im Scene Builder fertig, ohne Actions im Code

---
+++ Sushi-like-a-boss/src/main/java/com/example/sushilikeaboss/Recircle.java
@@ -0,0 +1,12 @@
+package com.example.sushilikeaboss;
+
+import java.util.ArrayList;
+
+public abstract class Recircle extends Sushi {
+
+
+    protected Recircle(int quantity) {
+        super("Depot Recircle Box", quantity, 10);
+    }
+
+}
